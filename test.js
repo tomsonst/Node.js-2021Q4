c737c18186bf14d0f20735378052dc0f39b91f22
MINSK\Stanislau_Tamashou <Stanislau_Tamashou@epam.com>
2021-11-22 08:40:59 +0300

    fixed problem with test

--- test.js
+++
@@ -1,9 +0,0 @@
-// const WriteStream = require('./write-Stream.js');
-// const { describe } = require('yargs');
-// const { test, expect } = require('@jest/globals');
-
-// describe('WriteStream', () => {
-//   test('should return correct object', () => {
-//     expect(new WriteStream('output.txt')).toEqual('zyx');
-//   })
-// });
